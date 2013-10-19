37dae66cff5e146b0ab999e498255082ad03d33c
aleksevd <aleks.evd@gmail.com>
2013-10-19 11:38:48 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hotfix::Application.config.secret_key_base = '7f026c41c66654c26c753a1be1ee39fdf8589aa2affd6739739e37160206564faa989bbbc791ec47b4cf53694c9b376b5876eb22da1ec66b7bf45e1b0adbb5c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hotfix::Application.config.session_store :cookie_store, key: '_hotfix_session'
