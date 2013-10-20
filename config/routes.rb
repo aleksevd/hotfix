2e0f44f146fd2fe2972a82cdb6ae06cb40060d68
Igor Sidorov <igor.cug@gmail.com>
2013-10-20 13:24:32 +0400

    Added draft layout for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Hotfix::Application.routes.draw do
-  root to: 'project_files#index'
+  root to: 'landings#show'
 
   get :project_files, to: 'project_files#index'
   get :project_file, to: 'project_files#show'
